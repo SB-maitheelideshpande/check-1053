dd3d70a321febacb853c4681becce104c106571c
yogesh.jadhav@smartbear.com <yogesh.jadhav@smartbear.com>
2024-09-24 12:17:11 +0530

    Added 7 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-19628.stoplight-dev.com/api/v1/projects/cHJqOjEyMzY/spectral.js?branch_slug=main&token=824a8437-fd96-41a0-8895-f2b721c0b1c8";
+export default { extends: ruleset };
